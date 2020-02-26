10df387802cee323ce8e8af6a1a0adc14a54e4b9
Behemoth86 <megadeth86@mail.ru>
2020-02-27 01:53:16 +0400

    feat: add functions min,max,avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (typeof array === 'undefined'|| array.length===0){return 0;}
+  else{ let len = array.length;
+        let min = array[0];
+        for(let i=1; i<len; i++){if (array[i]<min) {min=array[i];}}
+        return min;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (typeof array === 'undefined'|| array.length===0){return 0;}
+  else{ let len = array.length;
+        let max = array[0];
+        for(let i=1; i<len; i++){if (array[i]>max) {max=array[i];}}
+        return max;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (typeof array === 'undefined'|| array.length===0){return 0;}
+  else{ let znam = array.length;
+        let summa = 0;
+        for (let i=0; i<znam; i++) {summa += array[i];}
+        return (summa/znam);
+  }
 }
